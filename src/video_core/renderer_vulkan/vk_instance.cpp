9b89832293a21667457d6aff4e5dccc19d3f0084
Kravickas <joker46@seznam.cz>
2026-04-05 04:28:17 +0200

    Update vk_instance.cpp

--- src/video_core/renderer_vulkan/vk_instance.cpp
+++ src/video_core/renderer_vulkan/vk_instance.cpp
@@ -8,7 +8,6 @@
 #include "common/assert.h"
 #include "common/debug.h"
 #include "common/types.h"
-#include "imgui/renderer/imgui_core.h"
 #include "sdl_window.h"
 #include "video_core/renderer_vulkan/liverpool_to_vk.h"
 #include "video_core/renderer_vulkan/vk_instance.h"
@@ -184,7 +183,6 @@ Instance::Instance(Frontend::WindowSDL& window, s32 physical_device_index,
 }
 
 Instance::~Instance() {
-    ImGui::Core::Shutdown(GetDevice());
     vmaDestroyAllocator(allocator);
 }
 
@@ -406,6 +404,7 @@ bool Instance::CreateDevice() {
                 .shaderFloat64 = features.shaderFloat64,
                 .shaderInt64 = features.shaderInt64,
                 .shaderInt16 = features.shaderInt16,
+                .occlusionQueryPrecise = features.occlusionQueryPrecise,
             },
         },
         vk::PhysicalDeviceVulkan11Features{
