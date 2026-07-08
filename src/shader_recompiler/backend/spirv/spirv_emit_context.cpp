eff4f0b290d30033906cd9c57a5a79d064afca44
Kravickas <joker46@seznam.cz>
2026-07-08 02:59:39 +0200

    del depth

--- src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
@@ -922,8 +922,7 @@ spv::ImageFormat GetFormat(const AmdGpu::Image& image) {
 Id ImageType(EmitContext& ctx, const ImageResource& desc, Id sampled_type) {
     const auto image = desc.GetSharp(ctx.info);
     const auto format = desc.is_atomic ? GetFormat(image) : spv::ImageFormat::Unknown;
-    const auto type =
-        image.GetViewType(desc.is_array, desc.is_storage || desc.is_written || desc.is_depth);
+    const auto type = image.GetViewType(desc.is_array, desc.is_storage || desc.is_written);
     const u32 sampled = desc.is_written ? 2 : 1;
     switch (type) {
     case AmdGpu::ImageType::Color1D:
@@ -978,9 +977,8 @@ void EmitContext::DefineImagesAndSamplers() {
             .id = id,
             .sampled_type = is_storage ? sampled_type : TypeSampledImage(image_type),
             .image_type = image_type,
-            .view_type = sharp.GetViewType(image_desc.is_array, image_desc.is_storage ||
-                                                                    image_desc.is_written ||
-                                                                    image_desc.is_depth),
+            .view_type = sharp.GetViewType(image_desc.is_array,
+                                           image_desc.is_storage || image_desc.is_written),
             .is_integer = is_integer,
             .is_storage = is_storage,
             .mip_fallback_mode = mip_fallback_mode,
