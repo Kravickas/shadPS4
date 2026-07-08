e9ad1045d0d153632b56ef5e8b870c9d31623845
Kravickas <joker46@seznam.cz>
2026-07-08 03:02:56 +0200

    del depth sharp

--- src/shader_recompiler/specialization.h
+++ src/shader_recompiler/specialization.h
@@ -136,7 +136,7 @@ struct StageSpecialization {
         ForEachSharp(binding, images, info->images,
                      [&](auto& spec, const auto& desc, AmdGpu::Image sharp) {
                          spec.type = sharp.GetViewType(
-                             desc.is_array, desc.is_storage || desc.is_written || desc.is_depth);
+                             desc.is_array, desc.is_storage || desc.is_written);
                          spec.is_integer = AmdGpu::IsInteger(sharp.GetNumberFmt());
                          spec.is_storage = desc.is_written;
                          if (spec.is_storage) {
